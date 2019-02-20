9a3571b7e4b50811d2e43a6ae5a3aca8b7feae9e
Liyani Lustado <liyanilustado10@gmail.com>
2019-02-19 23:06:57 -0800

    Added snakeinfo.py

--- app/grid.py
+++ app/grid.py
@@ -2,23 +2,34 @@ import snakeinfo as si
 
 # Create the grid to track the python
 def createGrid(data):
-    board = si.boardDimension(data['board']['height'], data['board']['width'])
-    #board.board()
+    board = si.board(
+        data['board']['height'], 
+        data['board']['width'],
+        data['board']['food'],
+        data['board']['snakes']
+    )
+    board.printFood()
+
+    # Grid Position variable declaration
     head = 3
     snake = 4
     food = 1
-    width = (board.width)
-    #print(width)
-    height = (board.height)
-    #print(height)
+
+    # Class variable declarations
+    width = board.width
+    height = board.height
+    foodPos = board.food
+    allSnakes = board.snakes
+
+    
 
     grid = [[0 for col in range(width)] for row in range(height)]
     ## Plotting out the food in the grid
-    for eat in data['board']['food']:
+    for eat in foodPos:
         grid[eat['y']][eat['x']] = food
 
     ## Snakes placed in the grid 
-    for snakes in data['board']['snakes']:
+    for snakes in allSnakes:
         x = snakes['body'][0]['x']
         y = snakes['body'][0]['y']
         grid[y][x] = head    
--- app/snakeinfo.py
+++ app/snakeinfo.py
@@ -1,11 +1,39 @@
 
-# This function gets all the data from the json and sets them to variables for easy use 
-class boardDimension:
-    def __init__(self, height, width):
+# These classes get all the data from the json and sets them to variables for easy use 
+
+# This class deals with the board including all snakes and food
+class board:
+    def __init__(self, height, width, food, snakes):
         self.height = height
         self.width = width
+        self.food = food
+        self.snakes = snakes
 
-    def board(self):
+
+    def printBoard(self):
         print("Board is: ", self.height, " x ", self.width)
 
+    def printFood(self):
+        print("Food is at position(s): ", self.food)
 
+# This class deals with the heads of all snakes in the board
+class sHead:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def printHead(self):
+        print("Head is at: ", self.x, " : ", self.y)
+
+
+# This class deals with our snakes id, name, health, and body position
+class ourSnake:
+    def __init__(self, sid, name, health, x, y):
+        self.sid = sid
+        self.name = name
+        self.health = health
+        self.x = x
+        self.y = y
+    
+    def printHead(self):
+        print("Head is at: ", self.x, " : ", self.y)
