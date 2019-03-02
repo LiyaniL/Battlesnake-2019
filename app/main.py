298666b77d1bd7c021f510f1bd23d834d24d2e29
forestmvey <36905077+forestmvey@users.noreply.github.com>
2019-03-02 13:36:49 -0800

    commit prior to if/else logic

--- app/main.py
+++ app/main.py
@@ -51,13 +51,13 @@ def start():
 def move():
     data = bottle.request.json
     grid = createGrid(data)
-    generatePath(grid, data)
+    # generatePath(grid, data)
     # print (grid)
     # print(json.dumps(data, indent=4, sort_keys=True))
 
     directions = ['up', 'down', 'left', 'right']
 
-    direction = random.choice(directions)
+    direction = generatePath(grid, data)
 
     return move_response(direction)
 
--- app/move.py
+++ app/move.py
@@ -1,16 +1,15 @@
 # This will deal with the movement of the snake
 import snakeinfo as si
-import getFood
 from pathfinding.core.diagonal_movement import DiagonalMovement
 from pathfinding.core.grid import Grid
 from pathfinding.finder.a_star import AStarFinder
 directions = ['up', 'down', 'left', 'right']
 
 def generatePath(grid, data):
+
+
     grid = Grid(matrix=grid)
 
-    def getFood():
-        
     # Board class declaration
     board = si.board (
         data['board']['height'], 
@@ -45,9 +44,11 @@ def generatePath(grid, data):
 
     start = grid.node(ourX, ourY)
     end = grid.node(0, 0)
-    finder = AStarFinder(diagonal_movement=DiagonalMovement.never)
 
+    finder = AStarFinder(diagonal_movement=DiagonalMovement.never)
     path, runs = finder.find_path(start, end, grid)
     print(path[1])
     print ('operations: ', runs, 'path length: ', len(path))
     print(grid.grid_str(path=path, start=start, end=end))
+
+    return ()
